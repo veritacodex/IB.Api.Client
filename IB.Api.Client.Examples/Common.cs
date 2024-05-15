38f5836fccf70dfee8f5230c67915b23132d963f
AnAlgoTrader <verita.codex@gmail.com>
2024-05-15 20:39:20 +0200

    .

--- IB.Api.Client.Examples/Common.cs
+++ IB.Api.Client.Examples/Common.cs
@@ -3,8 +3,7 @@ using System;
 namespace IB.Api.Client.Examples
 {
     public static class Common
-    {
-        
+    {        
         public static void KeepConsoleAlive()
         {
             Console.ReadLine();
--- IB.Api.Client.Examples/Program.cs
+++ IB.Api.Client.Examples/Program.cs
@@ -13,8 +13,7 @@ namespace IB.Api.Client.Examples
                 Port = 4002,
                 ClientId = 0
             };
-            
-            RequestNews.RunGetNewsProviders(connectionDetails);
+            BasicConnection.Run(connectionDetails);
             Common.KeepConsoleAlive();
         }
     }
