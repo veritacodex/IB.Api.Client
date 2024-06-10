be441b5f65053c689616415ea530aff0f477fec5
AnAlgoTrader <verita.codex@gmail.com>
2024-06-10 15:27:15 +0200

    .

--- IB.Api.Client/Client/MarketData.cs
+++ IB.Api.Client/Client/MarketData.cs
@@ -63,7 +63,7 @@ namespace IB.Api.Client
         {
             _optionParameterDefinitions = [];
             Notify($"Derivatives parameters for symbol {contractDetails.Contract.Symbol} requested");
-            ClientSocket.ReqSecDefOptParams(reqId, contractDetails.Contract.Symbol, contractDetails.Contract.Exchange, contractDetails.Contract.SecType, contractDetails.Contract.ConId);
+            ClientSocket.ReqSecDefOptParams(reqId, contractDetails.Contract.Symbol, string.Empty, contractDetails.Contract.SecType, contractDetails.Contract.ConId);
         }
         public void UpdateMktDepth(int tickerId, int position, int operation, int side, double price, decimal size)
         {
